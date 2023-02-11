942a1f1b586977e98ce594056cc1e252132ee2af
Noor A Alam <109654291+Noor-A-Alam@users.noreply.github.com>
2023-02-11 15:12:57 +0000

    Ternary OP

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+//Ternary Operator
+
+#include <stdio.h>
+int main(void)
+{
+int a,b,c;
+printf("Enter three integer numbers: ");
+scanf("%d %d %d",&a,&b,&c);
+a>b?(b>c? printf("%d",b):printf("%d",c)):(c>a?printf("%d",c):printf("%d",a));
 }
